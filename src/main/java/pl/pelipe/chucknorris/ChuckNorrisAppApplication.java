98f6a9aa73bf5e063fe07c394a07c8633eab6ec0
pelipe-pl <filipandersen@outlook.com>
2019-11-17 21:32:42 +0100

    Refactoring to Spring XML configuration

--- src/main/java/pl/pelipe/chucknorris/ChuckNorrisAppApplication.java
+++ src/main/java/pl/pelipe/chucknorris/ChuckNorrisAppApplication.java
@@ -2,12 +2,13 @@ package pl.pelipe.chucknorris;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class ChuckNorrisAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChuckNorrisAppApplication.class, args);
     }
-
 }
